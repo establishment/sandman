ab63172697304a2756c67df67a4531fa5c74662e
Mihai Ciucu <mihai@blink.net>
2025-09-10 17:39:00 +0300

    Use cgroups v2

--- src/cgroups.hpp
+++ src/cgroups.hpp
@@ -1,8 +1,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/vfs.h>
 
 #include <string>
+#include <vector>
 
 #include "runstats.hpp"
 
@@ -10,20 +12,10 @@
 #include "cpp-base/os.hpp"
 #include "cpp-base/logger.hpp"
 
-/// CGroups API
+/// CGroups v2 API - simplified for unified hierarchy
 class CGroups {
   public:
-    /// A subsystem ⁠ represents a single resource, such as CPU time or memory
-    struct CGSubsystem {
-        std::string name;
-        bool optional;
-    };
-
-    static CGSubsystem cgMemory;
-    static CGSubsystem cgCpuacct;
-    static CGSubsystem cgCpuset;
-    static CGSubsystem cgAllSubSystems[3];
-    static std::string cgRootPath;   /// "/sys/fs/cgroup":
+    static std::string cgRootPath;   /// "/sys/fs/cgroup"
 
     CGroups() {
         this->cgName = "";
@@ -31,7 +23,7 @@ class CGroups {
         this->useCGTiming = 1;
     }
 
-    std::string cgName;              /// name of the control goup
+    std::string cgName;         /// name of the control group
     int cgMemoryLimitKB;        /// memory limit for that cg
     int useCGTiming;            /// query process time from control group - default = true
 
@@ -39,16 +31,16 @@ class CGroups {
     char buffer[kCGBufferSize];
 
   protected:
-    /// a parameter is a child of a subsystem, like memory.usage_in_bytes or cpuacct.usage under cpuacct
-    std::string getPath(CGSubsystem subsystem, const std::string& parameter) {
-        return Base::StrCat(cgRootPath, '/', subsystem.name, '/', cgName, '/', parameter);
+    /// Build path to cgroup v2 file: /sys/fs/cgroup/group_name/parameter
+    std::string getPath(const std::string& parameter) {
+        return Base::StrCat(cgRootPath, '/', cgName, '/', parameter);
     }
 
-    int readStat(CGSubsystem subsystem, const std::string& parameter, bool maybe=false) {
+    int readStat(const std::string& parameter, bool maybe=false) {
         int success = 0;
         int readSize = 0;
 
-        std::string path = getPath(subsystem, parameter);
+        std::string path = getPath(parameter);
 
         int fd = open(path.c_str(), O_RDONLY);
         if (fd < 0) {
@@ -93,13 +85,13 @@ class CGroups {
         return success;
     }
 
-    int writeStat(CGSubsystem subsystem, const std::string& parameter, const std::string& value, bool maybe=false) {
+    int writeStat(const std::string& parameter, const std::string& value, bool maybe=false) {
         int success = 0;
         ssize_t writeSize = 0;
 
         Base::Msg(2, "CG: Write %s = %s\n", parameter.c_str(), value.c_str());
 
-        std::string path = getPath(subsystem, parameter);
+        std::string path = getPath(parameter);
 
         /// check file descriptor
         int fd = open(path.c_str(), O_WRONLY | O_TRUNC);
@@ -142,6 +134,19 @@ class CGroups {
         if (!Base::DirExists(cgRootPath.c_str())) {
             Base::Die("Control group filesystem at %s not mounted", cgRootPath.c_str());
         }
+        
+        /// Verify we have cgroups v2
+        struct statfs fs;
+        if (statfs(cgRootPath.c_str(), &fs) < 0) {
+            Base::Die("Cannot stat cgroup filesystem at %s: %m", cgRootPath.c_str());
+        }
+        
+        // cgroup2 magic number is 0x63677270
+        if (fs.f_type != 0x63677270) {
+            Base::Die("Only cgroups v2 is supported. Found filesystem type: 0x%lx", fs.f_type);
+        }
+        
+        Base::Msg("Using cgroups v2\n");
     }
 
     void init(int cgId) {
@@ -149,91 +154,91 @@ class CGroups {
         Base::Msg("Using control group %s\n", cgName.c_str());
     }
 
-    /// creates cgroup
-    /// sets cpu accesibility (cores) and memory accesibility (RAM nodes) if any from parent process
+    /// creates cgroup and enables controllers
     void prepare() {
         Base::Msg("Preparing control group %s\n", cgName.c_str());
-        for (auto subsystem : cgAllSubSystems) {
-            struct stat st;
-            std::string path = getPath(subsystem, "");
-            if (stat(path.c_str(), &st) >= 0 || errno != ENOENT) {
-                Base::Msg("Control group %s already exists, trying to empty it.\n", path.c_str());
-                if (rmdir(path.c_str()) < 0) {
-                    Base::Die("Failed to reset control group %s: %m", path.c_str());
-                }
-            }
-
-            if (mkdir(path.c_str(), 0777) < 0 && !subsystem.optional) {
-                Base::Die("Failed to create control group %s: %m", path.c_str());
+        
+        // Create the cgroup directory
+        struct stat st;
+        std::string path = Base::StrCat(cgRootPath, '/', cgName);
+        if (stat(path.c_str(), &st) >= 0 || errno != ENOENT) {
+            Base::Msg("Control group %s already exists, trying to empty it.\n", path.c_str());
+            if (rmdir(path.c_str()) < 0) {
+                Base::Die("Failed to reset control group %s: %m", path.c_str());
             }
         }
 
-        /// If cpuset module is enabled, copy allowed cpus and memory nodes from parent group
-        /// limits the cpus which can be used by target process
-        /// usefull in a multi cored environment
-        if (readStat(cgCpuset, "cpuset.cpus", true)) { /// try to read
-            writeStat(cgCpuset, "cpuset.cpus", buffer); /// write what was just read
+        if (mkdir(path.c_str(), 0777) < 0) {
+            Base::Die("Failed to create control group %s: %m", path.c_str());
+        }
+        
+        // Enable controllers in v2 - try to enable at root level first
+        std::string controllers_path = Base::StrCat(cgRootPath, "/cgroup.subtree_control");
+        int fd = open(controllers_path.c_str(), O_WRONLY);
+        if (fd < 0) {
+            Base::Die("Cannot open cgroup.subtree_control: %m");
+        }
+        
+        vector<string> controllerNames = {"+memory", "+cpuset"};
+        for (const auto& controllerName : controllerNames) {
+            ssize_t written = write(fd, controllerName.c_str(), controllerName.length());
+            if (written < 0) {
+                close(fd);
+                Base::Die("Failed to enable controller %s: %m", controllerName.c_str());
+            } else {
+                Base::Msg("Successfully enabled controller %s\n", controllerName.c_str());
+            }
+        }
+        close(fd);
+
+        /// Copy CPU and memory configuration from parent
+        if (readStat("cpuset.cpus.effective", true)) {
+            writeStat("cpuset.cpus", buffer, true);
         }
 
-        /// sets RAM nodes which this program can use
-        /// can be specified to be the first 2 GB of ram for example
-        if (readStat(cgCpuset, "cpuset.mems", true)) { /// try to read
-            writeStat(cgCpuset, "cpuset.mems", buffer); /// write what was just read
+        if (readStat("cpuset.mems.effective", true)) {
+            writeStat("cpuset.mems", buffer, true);
         }
     }
 
     void enter() {
         Base::Msg("Entering control group %s\n", cgName.c_str());
 
-        for (auto subsystem : cgAllSubSystems) {
-            /// add current pid to be contored by the cgroup for all target subsystems
-            writeStat(subsystem, "tasks", Base::StrCat(getpid()), subsystem.optional);
-        }
+        // Add current process to cgroup
+        writeStat("cgroup.procs", Base::StrCat(getpid()));
 
         if (cgMemoryLimitKB) {
-            /// limit RAM used by cgroup
-            writeStat(cgMemory, "memory.limit_in_bytes", Base::StrCat(cgMemoryLimitKB << 10));
-
-            /// limit swap space used by cgroup if it can
-            writeStat(cgMemory, "memory.memsw.limit_in_bytes", Base::StrCat(cgMemoryLimitKB << 10), true);
-        }
-
-        /// time is queried from cgroup for the process. Reset the stats for that cgroup
-        if (useCGTiming) {
-            /// reset the value of cgroup consumed time
-            writeStat(cgCpuacct, "cpuacct.usage", "0");
+            // Set memory limit
+            writeStat("memory.max", Base::StrCat(cgMemoryLimitKB << 10));
+            writeStat("memory.swap.max", Base::StrCat(cgMemoryLimitKB << 10), true);
         }
     }
 
     /// removes cgroup
     void cleanup() {
-        /// search for each subsystem (memory, cpuset, acuacct) and delete them
-        for (auto subsystem : cgAllSubSystems) {
-            if (subsystem.optional) {
-                if (!readStat(subsystem, "tasks", true)) {
-                    continue;
-                }
-            } else {
-                readStat(subsystem, "tasks");
-            }
-
+        // Check if any processes are still in the cgroup
+        if (readStat("cgroup.procs", true)) {
             if (buffer[0]) {
-                Base::Die("Some tasks left in subsystem %s of cgroup %s, failed to remove it",
-                    subsystem.name.c_str(), cgName.c_str());
+                Base::Die("Some processes left in cgroup %s, failed to remove it", cgName.c_str());
             }
+        }
 
-            std::string path = getPath(subsystem, "");
-
-            if (rmdir(path.c_str()) < 0) {
-                Base::Die("Cannot remove control group %s: %m", path.c_str());
-            }
+        std::string path = Base::StrCat(cgRootPath, '/', cgName);
+        if (rmdir(path.c_str()) < 0) {
+            Base::Die("Cannot remove control group %s: %m", path.c_str());
         }
     }
 
     unsigned long long cpuTimeNs() {
-        readStat(cgCpuacct, "cpuacct.usage");
-        unsigned long long ns = atoll(buffer);
-        return ns;
+        // In v2, cpu.stat contains usage_usec
+        if (readStat("cpu.stat", true)) {
+            char* usage_line = strstr(buffer, "usage_usec ");
+            if (usage_line) {
+                unsigned long long usec = atoll(usage_line + strlen("usage_usec "));
+                return usec * 1000; // Convert microseconds to nanoseconds
+            }
+        }
+        return 0;
     }
 
     unsigned long long cpuTimeMs() {
@@ -245,41 +250,43 @@ class CGroups {
         timeStat.wallTimeMs = 0;
         timeStat.cpuTimeMs = cpuTimeMs();
 
-        /// http://stackoverflow.com/questions/3875801/convert-jiffies-to-seconds/6786757#6786757
-        /// returns in jiffies. jiffies/sec = 100. num_juffies / 100 * 1000 to ms
-        readStat(cgCpuacct, "cpuacct.stat");
-        timeStat.userTimeMs = 10 * atoll(strstr(buffer, "user ") + strlen("user "));
-        timeStat.systemTimeMs = 10 * atoll(strstr(buffer, "system ") + strlen("system "));
+        if (readStat("cpu.stat", true)) {
+            // v2 format: user_usec 12345\nsystem_usec 67890\n...
+            char* user_line = strstr(buffer, "user_usec ");
+            char* system_line = strstr(buffer, "system_usec ");
+            
+            if (user_line) {
+                unsigned long long userUsec = atoll(user_line + strlen("user_usec "));
+                timeStat.userTimeMs = userUsec / 1000; // Convert usec to ms
+            }
+            if (system_line) {
+                unsigned long long systemUsec = atoll(system_line + strlen("system_usec "));
+                timeStat.systemTimeMs = systemUsec / 1000; // Convert usec to ms
+            }
+        }
 
         return timeStat;
     };
 
     size_t memoryKB() {
-        // Memory usage statistics
-        size_t mem = 0, memsw = 0;
-        if (readStat(cgMemory, "memory.max_usage_in_bytes", true)) {
+        // Memory usage statistics from v2
+        size_t mem = 0;
+        if (readStat("memory.peak", true)) {
             mem = atoll(buffer);
         } else {
             Base::Msg("Failed to read memory from cgroup");
         }
 
-        if (readStat(cgMemory, "memory.memsw.max_usage_in_bytes", true)) {
-            memsw = atoll(buffer);
-            if (memsw > mem) {
-                mem = memsw;
+        // Try swap peak as well
+        if (readStat("memory.swap.peak", true)) {
+            size_t swap = atoll(buffer);
+            if (swap > mem) {
+                mem = swap;
             }
-        } else {
-            Base::Msg("Failed to read swap memory from cgroup");
         }
 
-        return mem >> 10;
+        return mem >> 10; // Convert bytes to KB
     }
 };
 
-CGroups::CGSubsystem CGroups::cgMemory = {"memory", false};
-CGroups::CGSubsystem CGroups::cgCpuacct = {"cpuacct", false};
-CGroups::CGSubsystem CGroups::cgCpuset = {"cpuset", true};
-
-CGroups::CGSubsystem CGroups::cgAllSubSystems[] = {cgMemory, cgCpuacct, cgCpuset};
-
-std::string CGroups::cgRootPath = "/sys/fs/cgroup";
+std::string CGroups::cgRootPath = "/sys/fs/cgroup/unified";
